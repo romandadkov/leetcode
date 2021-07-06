db6f2e69af5807d778c7b6d886fd62f9c97b711d
Roman Dadkov <r.dadkov@gmail.com>
2021-07-06 21:47:51 +0300

    leetcode 147: Insertion Sort List
    * 147 insertion-sort-list
    * md file modified

---
+++ algorithms/leetcode_0147.cpp
@@ -0,0 +1,34 @@
+/*
+147. Insertion Sort List
+https://leetcode.com/problems/insertion-sort-list/
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode* a = new ListNode(-1);
+        for (ListNode* cur = a; head;) {
+            ListNode* t = head->next;
+            
+            for (cur = a; cur->next && cur->next->val < head->val; cur = cur->next);
+            
+            head->next = cur->next;
+            cur->next = head;
+            
+            head = t;
+        }
+        
+        return a->next;
+    }
+};
